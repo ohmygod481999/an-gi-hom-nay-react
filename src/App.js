8995c95bf2b0426ddc496865bf722635e0ee7fcf
nguyendinhinit <48084269+nguyendinhinit@users.noreply.github.com>
2021-08-09 21:48:56 +0700

    Maintence page
    Maintence page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import GetStarted from "./pages/GetStarted";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./pages/Login";
+import Maintence from "./pages/maintence/Maintence";
+import BottomNav from "./layout/bottomnav/BottomNav";
 
 function App() {
     return (
         <Router>
             <Switch>
-                <Route exact path="/">
-                    <GetStarted />
+                <Route path={"/maintence"}>
+                    <Maintence/>
                 </Route>
                 <Route path="/login">
                     <Login />
                 </Route>
+                <Route exact path="/">
+                    <GetStarted />
+                </Route>
             </Switch>
         </Router>
+
     );
 }
 
